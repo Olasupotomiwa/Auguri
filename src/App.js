d97df5ee75f3a46b196a6789916681b26b677b1f
Olasupo Tomiwa <tomiwaolasupo@gmail.com>
2024-06-15 19:26:01 +0100

    Fixed live chat button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Container } from '@chakra-ui/react';
 import NavBar from './Homepage/NavBar';
 import Homepage from './Homepage';
 
 function App() {
   return (
+    <Container maxW="1200px">
     <>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        
-      </Routes>
+     
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+     
     </>
+    </Container>
   );
 }
 
--- src/Homepage/Hero.js
+++ src/Homepage/Hero.js
@@ -1,7 +1,11 @@
-import { Box, Flex, Heading, Image } from "@chakra-ui/react";
+import { Avatar, Box, Flex, Heading, Image, Text, HStack } from "@chakra-ui/react";
+import { ChatIcon } from '@chakra-ui/icons';
 import Bg from "../assets/BG.png";
 import Pics from "../assets/Pics.png";
 import Pics2 from "../assets/Pics2.png";
+import CAC  from "../assets/CAC.jfif"
+
+
 
 const Hero = () => {
   return (
@@ -14,9 +18,22 @@ const Hero = () => {
       py={8}
     >
       <Flex w="full" direction={{ base: "column", md: "row" }}>
-        <Box flex="1" textAlign='left'>
-          <Heading fontSize={{ base: "40px", md: "60px" }} fontWeight="bold">
-            Explore the world with{" "}
+        <Box flex="1" textAlign='left' mt={{base: '0', md: '14'}}>
+        <Heading fontSize={{ base: "40px", md: "60px" }} fontWeight="bold">
+            <Text as="span" position="relative" display="inline-block">
+              Explore the world
+              <Box
+                as="span"
+                position="absolute"
+                bottom={0}
+                left={0}
+                right={0}
+                height="8px"
+                bg="gold"
+                zIndex="-1"
+              />
+            </Text>{" "}
+            with{" "}
             <Box as="span" color="#9D7C49">
               AUGURI TRAVEL AGENCY &
             </Box>{" "}
@@ -33,6 +50,11 @@ const Hero = () => {
           >
             Where Adventure meets Experience
           </Heading>
+
+          <HStack>
+            <Avatar size='sm' src={CAC}/>
+            <Text fontWeight={"BOLD"} fontFamily={"Poppins"} fontSize={'18px'}>Registered by CAC</Text>
+          </HStack>
         </Box>
 
         <Box flex="1" mt={{ base: 10, md: 0 }} display="flex">
@@ -50,6 +72,41 @@ const Hero = () => {
           </Box>
         </Box>
       </Flex>
+
+      <Box pos={'relative'}>
+      <Box
+      position="fixed"
+      bottom={8}
+      right={8}
+      bg="#9D7C49"
+      color="white"
+      borderRadius="50%"
+      width={{base: '80px', md: '100px'}}
+      height={{base: '80px', md: '100px'}}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      boxShadow="lg"
+      cursor="pointer"
+      zIndex="999"
+      fontFamily={"Poppins"}
+      fontWeight={"600"}
+      animation="bubble 3s infinite"
+     
+    >
+      <Flex flexDirection="column" alignItems="center" >
+      <ChatIcon boxSize={{base: '4', md: '6'}} />
+        <Text fontSize="sm" fontWeight="bold" textAlign="center">
+          24/7
+        </Text>
+        <Text fontSize="xs" textAlign="center">
+          Live Chat
+        </Text>
+      </Flex>
+    </Box>
+    </Box>
+
+    
     </Flex>
   );
 };
