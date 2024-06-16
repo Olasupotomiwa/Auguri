18dfeec91dcced6b81f37eeff106ef2dc8fc6fcf
Olasupo Tomiwa <tomiwaolasupo@gmail.com>
2024-06-16 09:49:58 +0100

    Removed carousel

--- src/Homepage/TripOptions.js
+++ src/Homepage/TripOptions.js
@@ -1,9 +1,7 @@
 import { Box, Flex, Image, useBreakpointValue } from "@chakra-ui/react";
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import Business from '../assets/Business.png'
-import Education from '../assets/Education.png'
-import Vacation from '../assets/Vacation.png'
+import Business from '../assets/Business.png';
+import Education from '../assets/Education.png';
+import Vacation from '../assets/Vacation.png';
 
 const TripOptions = () => {
   const isMobile = useBreakpointValue({ base: true, md: false });
@@ -15,29 +13,23 @@ const TripOptions = () => {
   ];
 
   return (
-    <Box>
-      {isMobile ? (
-        <Carousel showThumbs={false} showStatus={false} infiniteLoop autoPlay>
-          {images.map((image, index) => (
-            <Box key={index}>
-              <Image src={image.src} alt={image.alt}  objectFit={"cover"} borderRadius="full" />
-            </Box>
-          ))}
-        </Carousel>
-      ) : (
-        <Flex direction="row" align="center" justify="center" gap={4}>
-          {images.map((image, index) => (
-            <ImageBox key={index} imageSrc={image.src}  objectFit={"cover"} />
-          ))}
-        </Flex>
-      )}
-    </Box>
+    <Flex
+      direction="row"
+      align="center"
+      justify="center"
+      wrap={isMobile ? "wrap" : "nowrap"}
+      gap={4}
+    >
+      {images.map((image, index) => (
+        <ImageBox key={index} imageSrc={image.src} />
+      ))}
+    </Flex>
   );
 };
 
 const ImageBox = ({ imageSrc }) => (
   <Box>
-    <Image src={imageSrc} w={"419px"} objectFit={"cover"} />
+    <Image src={imageSrc} w={"188px"} objectFit={"cover"} borderRadius="full" />
   </Box>
 );
 
