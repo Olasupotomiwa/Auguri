c36c46e2993b59d3dff8d70ab95d42b323aaff30
Olasupo Tomiwa <tomiwaolasupo@gmail.com>
2024-06-16 14:05:36 +0100

    Changed testimonial custom dot

--- src/Homepage/HeroCustomDot.js
+++ src/Homepage/HeroCustomDot.js
@@ -1,14 +1,16 @@
-// HeroCustomDot.jsx
 import React from "react";
-import { Box, Circle } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 const HeroCustomDot = ({ onClick, isActive }) => {
   return (
     <Box p={2} cursor="pointer" onClick={onClick}>
-      <Circle
-        size="12px"
+      <Box
+        width="20px"
+        height="3px"
         bg={isActive ? "#DF6951" : "#e6e6e6"}
+        borderRadius="2px"
         _hover={{ opacity: 0.7 }}
+        transition="background-color 0.3s"
       />
     </Box>
   );
